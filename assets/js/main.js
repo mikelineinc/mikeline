02293baad25f14d435c09cb95ac18fe7875269a8
mikelineinc <80030206+mikelineinc@users.noreply.github.com>
2022-01-25 22:42:04 +0600

    er

--- assets/js/main.js
+++ assets/js/main.js
@@ -67,8 +67,8 @@
 
     let days = Math.floor(timeleft / (100 * 60 * 60 * 24));
     let hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (100 * 60));
-    let seconds = Math.floor((timeleft % (1000 * 60)) / 100);
+    let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
 
     countdown.innerHTML = output.replace('%d', days).replace('%h', hours).replace('%m', minutes).replace('%s', seconds);
   }
